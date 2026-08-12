1d455010846b8d972a6da1602acd7086d5391f95
Kirti Kumar <kirtikumar237823@gmail.com>
2026-08-12 22:52:53 +0530

    Add files via upload

---
+++ Program 4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program_4
+    {
+        static void Main4(string[] args)
+        {
+            Console.Write("Enter the number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(n + " X " + i + " = " + (n * i));
+            }
+        }
+    }
+}
